694cd3ab5475b7cc28ae2ae78ace622a708ad7ea
akohas <doctorfreud33@gmail.com>
2017-11-06 21:34:38 +0300

    first commit

---
+++ src/client/App.jsx
@@ -0,0 +1,16 @@
+// @flow
+
+import React from 'react';
+import './App.css';
+import styled from 'styled-components'
+
+const Main = styled.div`
+  background: blue;
+`
+function App() {
+  return (
+    <Main>main</Main>
+  );
+}
+
+export default App;
